3be8465c83ca129c4da5a40be576af29b49f24de
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2019-02-19 15:54:03 +0200

    Added component for previewing a blog post which gets rendererd through md.

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
 const DateFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
+class Article {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.publishDate = new Date();
+        this.content = "#Heya\r\n*foo\r\n*bar\r\n![puppy](resources/puppy.jpg)";
+    }
+    get articleUrl() {
+        return this.title.toLowerCase().replace(/ /g, '%20');
+    }
+    get renderedContent() {
+        const options = {strikethrough: true, tasklists: true, openLinksInNewWindow: true, omitExtraWLInCodeBlocks: true, tables: true};
+        return new showdown.Converter(options).makeHtml(this.content);
+    }
+}
+
 let model = {
     profile: {
         photo: "https://dimitroff.bg/cv/images/portrait.png",
         name: "Nikola Dimitroff",
         description: "Ulalala"
     },
-    articles: [{
-            title: "Foo",
-            description: "Foodilalalala",
-            publishDate: new Date()
-        },{
-            title: "Foo",
-            description: "Foodilalalala",
-            publishDate: new Date()
-        },{
-            title: "Foo",
-            description: "Foodilalalala",
-            publishDate: new Date()
-        },{
-            title: "Foo",
-            description: "Foodilalalala",
-            publishDate: new Date()
-        },
-    ]
+    articles: [
+        new Article("Foo", "Foodilalallala"),
+        new Article("Foo", "Foodilalallala"),
+        new Article("Foo", "Foodilalallala"),
+        new Article("Foo", "Foodilalallala"),
+    ],
+    getArticleByUrl: function (router) {
+        return {
+            article: this.articles.find(a => a.articleUrl === router.params.url)
+        };
+    }
 };
 
 function registerComponents() {
@@ -41,9 +50,17 @@ function main() {
     Vue.config.devtools = true;
     registerComponents();
 
+    const articleComponent = Vue.options.components["article-display"];
+    const router = new VueRouter({
+        routes: [
+            { path: '/article/:url', component: articleComponent, props: model.getArticleByUrl.bind(model) },
+        ]
+    });
+
     window.app = new Vue({
         el: "#vue-container",
-        data: model
+        data: model,
+        router: router
     });
 }
 
